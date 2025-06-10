b02c27529328b52448c9f76cbb2df14834364663
AdamBalski <adambalski1@gmail.com>
2025-06-10 15:49:25 +0200

    fix websocket security and PrincipalUtils

--- src/main/java/com/messenger/javamessenger/util/PrincipalUtils.java
+++ src/main/java/com/messenger/javamessenger/util/PrincipalUtils.java
@@ -1,12 +1,16 @@
 package com.messenger.javamessenger.util;
 
 import com.messenger.javamessenger.model.UserEntity;
+import com.messenger.javamessenger.security.UserDetailsWithEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PrincipalUtils {
-    public UserEntity getPrincipal() {
-        return (UserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public UserEntity getCurrentUserEntity() {
+        return getPrincipal().getUser();
+    }
+    public UserDetailsWithEntity getPrincipal() {
+        return (UserDetailsWithEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
