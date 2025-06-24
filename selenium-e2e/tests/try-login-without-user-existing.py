3488ee96da0c90dedf0f82841103ef524a259977
AdamBalski <adambalski1@gmail.com>
2025-06-24 22:01:34 +0200

    update hostnames in e2e test

--- selenium-e2e/tests/try-login-without-user-existing.py
+++ selenium-e2e/tests/try-login-without-user-existing.py
@@ -7,7 +7,7 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(options=options)
-driver.get("http://frontend:3000")  # container name
+driver.get("http://localhost:3000")  # container name
 print(driver.title)
 driver.quit()
 
