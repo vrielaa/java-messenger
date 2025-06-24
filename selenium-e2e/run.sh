962ca927bbaeef62bc3de755930cac6bd294465d
AdamBalski <adambalski1@gmail.com>
2025-06-24 21:35:34 +0200

    add selenium action

---
+++ selenium-e2e/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+OK=0
+for file in tests/*; do
+    python3 "$file"
+    CURR_CODE=$?
+    if [ "$OK" = 0 ]; then
+        OK="$CURR_CODE"
+    fi
+done
+exit $OK
---
+++ selenium-e2e/tests/register-and-login.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(options=options)
+driver.get("http://frontend:3000")  # container name
+print(driver.title)
+driver.quit()
+
