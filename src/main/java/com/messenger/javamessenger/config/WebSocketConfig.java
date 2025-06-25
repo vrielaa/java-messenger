d9d30f48faeb9dab48c0828a02d1b782dbf01fd0
vriela <gabriela.fron04@gmail.com>
2025-06-25 17:41:24 +0200

    poprawa bledu( w WebSocketConfig.java zabraklo package com.messenger.javamessenger.config;)

--- src/main/java/com/messenger/javamessenger/config/WebSocketConfig.java
+++ src/main/java/com/messenger/javamessenger/config/WebSocketConfig.java
@@ -1,3 +1,5 @@
+package com.messenger.javamessenger.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
